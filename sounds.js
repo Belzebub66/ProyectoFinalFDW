8fb77a2551fcca43d2898771efb2f56711838a76
Leviathan <l23920238@voaxaca.tecnm.mx>
2025-03-03 00:12:43 -0600

    sound

--- sounds.js
+++ sounds.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const contenedor = document.querySelector('.card-containerS');
-    const sonido = document.getElementById('miSonido');
+    const contenedor = document.querySelector('.blog-card');
+    const sonido = document.getElementById('card-sound');
 
     contenedor.addEventListener('mouseenter', function() {
         sonido.play();
     });
+    contenedor.addEventListener('mouseleave', function() {
+        sonido.pause();
+        sonido.currentTime = 0; // Reinicia el sonido para la próxima vez
+    });
+});
