ef1ab009cb7b88ce638c944719b36972f1c75e31
Leviathan <l23920238@voaxaca.tecnm.mx>
2025-03-03 00:42:20 -0600

    duck

--- sounds.js
+++ sounds.js
@@ -3,8 +3,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const contenedores = document.querySelectorAll('.blog-card');
     const sonido = document.getElementById('card-sound');
 
+    const sonidos = [
+        'https://cdn.freesound.org/previews/369/369960_6857950-lq.mp3',
+        'https://cdn.freesound.org/previews/699/699296_5872390-lq.mp3',
+        'https://cdn.freesound.org/previews/3/3526_7212-lq.mp3',
+        'https://cdn.freesound.org/previews/614/614079_2476748-lq.mp3'
+    ];
+
     contenedores.forEach(contenedor => {
         contenedor.addEventListener('mouseenter', function() {
+            // Elige un sonido aleatorio de la lista
+            const sonidoAleatorio = sonidos[Math.floor(Math.random() * sonidos.length)];
+            sonido.src = sonidoAleatorio; // Cambia la fuente del audio
             sonido.currentTime = 0; // Reinicia el sonido para que se reproduzca desde el inicio
             sonido.play();
         });
